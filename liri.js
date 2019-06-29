517d1a80002cd60b9a1575987488f9db452589f2
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-06-29 15:39:40 -0600

    spotify-this makes more sense, do-what-it-says Actually functions now

--- liri.js
+++ liri.js
@@ -1,16 +1,18 @@
+require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api")
+var spotify = new Spotify(keys.spotify)
 var axios = require("axios");
 var fs = require("fs");
 var userInput = process.argv[2];
-var searchConcert = process.argv.slice(3).join("%20");
+var search = process.argv.slice(3).join("%20");
 var display = process.argv.slice(3).join(" ");
 
 
-
 //Function for concert-this
-function concert() {
+function concert(display) {
     console.log(`\n\nUpcoming concerts for ${display}\n\n`);
-    axios.get("https://rest.bandsintown.com/artists/" + searchConcert + "/events?app_id=codingbootcamp").then(function (response) {
+    axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp").then(function (response) {
         for (var i = 0; i < response.data.length; i++) {
             console.log(`Venue: ${response.data[i].venue.name}\nLocation: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}\nDate: ${response.data[i].datetime}\n\n`);
         };
@@ -18,14 +20,22 @@ function concert() {
 };
 
 
-
-function spotify() {
-    axios.get("718d7459ef3246c1a5c07b4410bdc28e").then(
-        function (response) {
-            console.log(response.data);
+function spotifyThis(info) {
+    spotify.search({
+        type: 'track',
+        query: info
+        
+    },
+    function (err, data) {
+        if (err) {
+            return console.log(err);
         }
-    )
+
+        console.log(data.tracks.items[0]);
+    }
+    ); 
 }
+
 
 
 function movie() {
@@ -34,7 +44,6 @@ function movie() {
 
     axios.get(queryUrl).then(
         function (response) {
-            // console.log(response.data);
             var data = response.data;
             console.log(`Title: ${data.Title}\nYear: ${data.Year}\nIMDB Rating: ${data.imdbRating}\nRotten Tomatoes: ${data.Ratings[1].Value}\nCountry: ${data.Country}\nLanguage: ${data.Language}\nPlot: ${data.Plot}`);
         })
@@ -46,17 +55,35 @@ function doThis() {
         if (err) {
             return console.log(err);
         }
+
         var dataArray = data.split(", ");
+        var action = dataArray[0];
+        var search = dataArray[1];
         console.log(dataArray);
+
+        switch (action) {
+            case "spotify-this-song":
+                spotifyThis(search);
+                break;
+            case "concert-this":
+                concert(search);
+                break;
+            case "movie-this":
+                movie(search);
+                break;
+            default:
+                console.log("Please input proper command")
+        }
     })
+
 }
 
 switch (userInput) {
     case "concert-this":
-        concert();
+        concert(display);
         break;
     case "spotify-this-song":
-        spotify();
+        spotifyThis(display);
         break;
     case "movie-this":
         movie();
