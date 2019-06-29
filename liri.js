c8aed68998e513ef9c6f1ece132f04729118ab20
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-06-28 20:14:23 -0600

    concert-this functions

--- liri.js
+++ liri.js
@@ -2,22 +2,22 @@ var keys = require("./keys.js");
 var axios = require("axios");
 var fs = require("fs");
 var userInput = process.argv[2];
+var searchConcert = process.argv.slice(3).join("%20");
+var display = process.argv.slice(3).join(" ");
 
 
 
 //Function for concert-this
 function concert() {
-    var concert = process.argv.slice(3).join("+");
-    var queryUrl = "https://rest.bandsintown.com/artists/" + concert + "/events?app_id=codingbootcamp"
-    axios.get(queryUrl).then(
-        function (response) {
-            console.log(response.data);
-            var data = response.data
-            if(concert){
-                console.log(`Name of the Venue: ${data.offers[2]} Venue Location: ${data.offers[5]}, ${data.offers[3]} Data: ${data.offers[8]}`)
-            }
-        })
-}
+    console.log(`\n\nUpcoming concerts for ${display}\n\n`);
+    axios.get("https://rest.bandsintown.com/artists/" + searchConcert + "/events?app_id=codingbootcamp").then(function (response) {
+        for (var i = 0; i < response.data.length; i++) {
+            console.log(`Venue: ${response.data[i].venue.name}\nLocation: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}\nDate: ${response.data[i].datetime}\n\n`);
+        };
+    });
+};
+
+
 
 function spotify() {
     axios.get("718d7459ef3246c1a5c07b4410bdc28e").then(
@@ -31,12 +31,12 @@ function spotify() {
 function movie() {
     var movie = process.argv.slice(3).join("+") || "Mr.+Nobody";
     var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-    
+
     axios.get(queryUrl).then(
         function (response) {
             // console.log(response.data);
             var data = response.data;
-                console.log(`Title: ${data.Title}\nYear: ${data.Year}\nIMDB Rating: ${data.imdbRating}\nRotten Tomatoes: ${data.Ratings[1].Value}\nCountry: ${data.Country}\nLanguage: ${data.Language}\nPlot: ${data.Plot}`);
+            console.log(`Title: ${data.Title}\nYear: ${data.Year}\nIMDB Rating: ${data.imdbRating}\nRotten Tomatoes: ${data.Ratings[1].Value}\nCountry: ${data.Country}\nLanguage: ${data.Language}\nPlot: ${data.Plot}`);
         })
 }
 
